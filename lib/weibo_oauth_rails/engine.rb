70bf219e4bfef5ca37708a31c1fd8ea67a18803e
simsicon <simsicon@gmail.com>
2012-05-17 18:34:11 +0800

    util

--- lib/weibo_oauth_rails/engine.rb
+++ lib/weibo_oauth_rails/engine.rb
@@ -1,5 +1,9 @@
 module WeiboOauthRails
   class Engine < ::Rails::Engine
     isolate_namespace WeiboOauthRails
+    
+    initializer do |app|
+      require File.expand_path('../utils', __FILE__)
+    end
   end
 end
