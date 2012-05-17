ec59c81a18fa45eda9923a08a39b72a0162582ce
simsicon <simsicon@gmail.com>
2012-05-17 18:48:30 +0800

    add argument

--- lib/weibo_oauth_rails/engine.rb
+++ lib/weibo_oauth_rails/engine.rb
@@ -2,7 +2,7 @@ module WeiboOauthRails
   class Engine < ::Rails::Engine
     isolate_namespace WeiboOauthRails
     
-    initializer do |app|
+    initializer "weibo_oauth_rails_engine.load_configs" do |app|
       require File.expand_path('../utils', __FILE__)
     end
   end
