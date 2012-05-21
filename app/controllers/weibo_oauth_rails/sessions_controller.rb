5be04fac75e8cebc5f44c4ea0dc485f357cd50ed
simsicon <simsicon@gmail.com>
2012-05-21 14:49:55 +0800

    create process

--- app/controllers/weibo_oauth_rails/sessions_controller.rb
+++ app/controllers/weibo_oauth_rails/sessions_controller.rb
@@ -33,5 +33,25 @@ module WeiboOauthRails
     def authenticate
       redirect_to :signin unless signed_in?
     end
+    
+    def create_with_omniauth(auth)
+      User.create! do |user|
+        user.provider = auth['provider']
+        user.uid = auth['uid']
+        if auth['info']
+          user.name = auth['info']['name'] || ""
+          user.email = auth['info']['email'] || ""
+        end
+      end
+    end
+    
+    def authenticate_with_cookie(id, cookie)
+      user = User.find(id) if id
+      (user && user.uid == cookie) ? user : nil
+    end
+
+    def cookie
+      uid
+    end
   end
 end
--- lib/weibo_oauth_rails/utils.rb
+++ lib/weibo_oauth_rails/utils.rb
@@ -1,26 +1,10 @@
 module WeiboOauthRails
   module Utils
     module InstanceMethods
-      def create_with_omniauth(auth)
-        create! do |user|
-          user.provider = auth['provider']
-          user.uid = auth['uid']
-          if auth['info']
-            user.name = auth['info']['name'] || ""
-            user.email = auth['info']['email'] || ""
-          end
-        end
-      end
+      
 
 
-      def authenticate_with_cookie(id, cookie)
-        user = find(id) if id
-        (user && user.uid == cookie) ? user : nil
-      end
-
-      def cookie
-        uid
-      end
+      
     end
   end
 end
