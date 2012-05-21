095e6c967981659b051450e9b631456d2116ea9a
simsicon <simsicon@gmail.com>
2012-05-21 14:40:17 +0800

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,9 @@
 WeiboOauthRails::Engine.routes.draw do
+  match '/auth/failure' => 'sessions#failure'
+
+  match '/signout' => 'sessions#destroy', :as => :signout
+
+  match '/signin' => 'sessions#new', :as => :signin
+
+  match '/auth/:provider/callback' => 'sessions#create'  
 end
