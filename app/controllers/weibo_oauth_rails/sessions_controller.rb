14adfc4c928ab262473948be2357cfc6c52fcce9
simsicon <simsicon@gmail.com>
2012-05-21 14:58:13 +0800

    remove USER

--- app/controllers/weibo_oauth_rails/sessions_controller.rb
+++ app/controllers/weibo_oauth_rails/sessions_controller.rb
@@ -9,7 +9,7 @@ module WeiboOauthRails
     def create
       auth = request.env["omniauth.auth"]
       user = User.where(:provider => auth['provider'], 
-                        :uid => auth['uid']).first || User.create_with_omniauth(auth)
+                        :uid => auth['uid']).first || create_with_omniauth(auth)
       sign_in(user)
       # if !user.email
       #       redirect_to edit_user_path(user), :alert => "Please enter your email address."
